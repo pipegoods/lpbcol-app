8f2748c14c34901a0062dadf391a7cbafc05d754
pipegoods <pipe.jaider@gmail.com>
2023-01-18 08:53:40 -0500

    add final serie table and twitter feed

--- src/services/leaderboard.ts
+++ src/services/leaderboard.ts
@@ -10,3 +10,8 @@ export const getSemifinal = async (): Promise<ISemifinal[]> => {
   const response = await fetch(`${apiURL}/semifinal`)
   return await response.json()
 }
+
+export const getFinal = async (): Promise<ISemifinal[]> => {
+  const response = await fetch(`${apiURL}/final`)
+  return await response.json()
+}
